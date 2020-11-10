673a2ba362d910f8d4f2ac325ff3abb7d7a82b28
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-11-10 19:12:27 +0300

    Begin L55. Strategy Design Pattern

---
+++ section09-design-patterns-in-functional-way/src/main55_strategy/StrategyPattern.java
@@ -0,0 +1,18 @@
+package main55_strategy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StrategyPattern {
+
+	public static void main(String[] args) {
+		List<Stock> stockList = new ArrayList<>();
+		stockList.add(new Stock("AAPL", 318.65, 10));
+		stockList.add(new Stock("MSFT", 166.86, 45));
+		stockList.add(new Stock("Google", 99, 12.5));
+		stockList.add(new Stock("AMZ", 1866.74, 45));
+		stockList.add(new Stock("GOOGL", 1480.20, 3.5));
+		stockList.add(new Stock("AAPL", 318.65, 8));
+		stockList.add(new Stock("AMZ", 1866.74, 9));
+	}
+}
