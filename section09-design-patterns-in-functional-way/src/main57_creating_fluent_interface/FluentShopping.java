e65f0f3d6d1c11ec6077bad1c70c2cc8d3b7a302
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-11-11 10:50:47 +0300

    Run FluentShopping.

--- section09-design-patterns-in-functional-way/src/main57_creating_fluent_interface/FluentShopping.java
+++ section09-design-patterns-in-functional-way/src/main57_creating_fluent_interface/FluentShopping.java
@@ -3,7 +3,12 @@ package main57_creating_fluent_interface;
 public class FluentShopping {
 
 	public static void main(String[] args) {
+		Order myOrder = new Order();
+		myOrder.add("Shoes");
+		myOrder.add("Headphones");
+		myOrder.deliverAt("Street no 45, Jodhpur");
 		
+		myOrder.place();
 	}
 	
 }
