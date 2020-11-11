e3a79a97d992a1348a759ed8c95154da5f25408d
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-11-11 13:52:04 +0300

    Implement Command using Lambda

--- section09-design-patterns-in-functional-way/src/main60_command/CommandPatternDemo.java
+++ section09-design-patterns-in-functional-way/src/main60_command/CommandPatternDemo.java
@@ -3,7 +3,15 @@ package main60_command;
 public class CommandPatternDemo {
 
 	public static void main(String[] args) {
-
+		AC ac1 = new AC();
+		
+		ACAutomationRemote remote = new ACAutomationRemote();
+		remote.setCommand(() -> ac1.turnOn());
+		remote.buttonPressed();
+		
+		ACAutomationRemote remote2 = new ACAutomationRemote();
+		remote2.setCommand(() -> ac1.turnOff());
+		remote2.buttonPressed();
 	}
 
 }
