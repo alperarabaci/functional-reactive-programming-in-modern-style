3bd406f1955da2d219ac3c8092733688c65ffb56
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-11-11 17:27:06 +0300

    Add IntStream, DoubleStream, LongStream examples…

--- section10-streams-and-parallel-streams-in-depth/src/main71_numeric_streams/NumericStreams.java
+++ section10-streams-and-parallel-streams-in-depth/src/main71_numeric_streams/NumericStreams.java
@@ -3,6 +3,10 @@ package main71_numeric_streams;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.OptionalDouble;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 import main62_strams_introduction.Book;
 
@@ -26,5 +30,14 @@ public class NumericStreams {
 		
 		//map: Object
 		//mapToDouble: primitive Double
+		
+		IntStream intStream = IntStream.of(1, 3, 5, 8); 
+		DoubleStream doubleStream = DoubleStream.of(1, 3, 5, 8); 
+		LongStream longStream = LongStream.of(1, 3, 5, 8);
+		
+		//
+		Stream<Integer> boxedInts = intStream.boxed();
+		//
+		Stream<Object> streamm = doubleStream.mapToObj(val -> val);
 	}
 }
