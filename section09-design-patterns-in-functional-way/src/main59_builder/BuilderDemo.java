388169ab20832da1ff08da84818ef35b66ad5a75
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-11-11 13:19:00 +0300

    Add comment to builder.

--- section09-design-patterns-in-functional-way/src/main59_builder/BuilderDemo.java
+++ section09-design-patterns-in-functional-way/src/main59_builder/BuilderDemo.java
@@ -5,6 +5,12 @@ public class BuilderDemo {
 	public static void main(String[] args) {
 		MobileBuilder builder = new MobileBuilder();
 		
+		/**
+		 * I prefer classic builder instead of this.
+		 * Check:
+		 * https://github.com/alperarabaci/design-patterns/blob/master/s04-builder/src/main/java/com/training/builderv2/ClientV2.java
+		 * 
+		 */
 		Mobile myMobile = builder.with( myBuilder -> {
 			myBuilder.ram = 4;
 			myBuilder.battery = 4000;
