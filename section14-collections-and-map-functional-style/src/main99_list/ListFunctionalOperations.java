427403322fec17dda244c7f413fbd918c3416eea
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-11-12 17:30:27 +0300

    Begin 99. List
    Functional way.

---
+++ section14-collections-and-map-functional-style/src/main99_list/ListFunctionalOperations.java
@@ -0,0 +1,21 @@
+package main99_list;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class ListFunctionalOperations {
+
+	public static void main(String[] args) {
+
+		List<Movie> movies = Arrays.asList(new Movie("Spotlight", 2015, "Hollywood"),
+				new Movie("Avengers: Infinity War", 2018, "Hollywood"),
+				new Movie("Inception", 2010, "Hollywood"),
+				new Movie("Forest Gump", 1994, "Hollywood"), 
+				new Movie("3 Idiots", 2009, "Bollywood"),
+				new Movie("Beauty and the beast", 2017, "Hollywood"),
+				new Movie("Slumdog Millionaire", 2008, "Bollywood"));
+
+	}
+
+}
