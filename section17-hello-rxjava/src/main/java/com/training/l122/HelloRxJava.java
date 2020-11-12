90a90b5760851da399f249b847407db9488018d0
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-11-12 22:29:07 +0300

    L121. Setting Up RxJava 3.0 L122. Hello - RxJava

---
+++ section17-hello-rxjava/src/main/java/com/training/l122/HelloRxJava.java
@@ -0,0 +1,26 @@
+package com.training.l122;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class HelloRxJava {
+
+	public static void main(String[] args) {
+
+		/**
+		 * Everything is synchronous we can't go async or concurrent until unless we don't actually ask for it.
+		 * Check console log.
+		 */
+		Observable<String> source = Observable.create(e -> {
+			e.onNext("Hello");
+			e.onNext("RxJava");
+			e.onNext("World");
+		});
+		
+		source.subscribe(e -> System.out.println("Observer 1: " +e));
+		source.subscribe(e -> System.out.println("Observer 2: " +e));
+		source.subscribe(e -> System.out.println("Observer 3: " +e));
+		source.subscribe(e -> System.out.println("Observer 4: " +e));
+		source.subscribe(e -> System.out.println("Observer 5: " +e));
+	}
+
+}
