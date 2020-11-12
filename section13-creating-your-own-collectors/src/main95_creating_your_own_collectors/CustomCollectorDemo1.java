1a0837c8a1a586e4baf1dcdfa6384ddc05287f92
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-11-12 17:00:05 +0300

    L96. Creating Your Own Collectors - 2
    Sort collection.

--- section13-creating-your-own-collectors/src/main95_creating_your_own_collectors/CustomCollectorDemo1.java
+++ section13-creating-your-own-collectors/src/main95_creating_your_own_collectors/CustomCollectorDemo1.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collector;
 
-public class CustomCollector {
+public class CustomCollectorDemo1 {
 
 	public static void main(String[] args) {
 		
