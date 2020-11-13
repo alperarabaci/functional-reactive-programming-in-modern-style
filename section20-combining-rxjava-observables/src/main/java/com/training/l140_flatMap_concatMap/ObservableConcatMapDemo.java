7d9ac9d9d7ad61f1febc8a38d4285beae7264341
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-11-13 16:57:37 +0300

    L140. flatMap() V/s concatMap()

---
+++ section20-combining-rxjava-observables/src/main/java/com/training/l140_flatMap_concatMap/ObservableConcatMapDemo.java
@@ -0,0 +1,27 @@
+package com.training.l140_flatMap_concatMap;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class ObservableConcatMapDemo {
+
+	public static void main(String[] args) {
+		List<String> list = List.of("Hello", "Reactive", "Programming");
+		
+		System.out.println("- concatMap");
+		//concatMap
+		/**
+		 *  now if i write concat map here
+			i will get the same output as the only difference
+			between concat map and flat map is that when we perform
+			merging using flat map on concurrently running
+			observables returned by this mapper function
+			then we get output 
+		 */
+		Observable.fromIterable(list)
+		.concatMap(e -> Observable.fromArray(e.split("")))
+		.subscribe(System.out::println);
+	}
+	
+}
---
+++ section20-combining-rxjava-observables/src/main/java/com/training/l140_flatMap_concatMap/ObservableFlatMapDemo.java
@@ -0,0 +1,25 @@
+package com.training.l140_flatMap_concatMap;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class ObservableFlatMapDemo {
+
+	public static void main(String[] args) {
+		List<String> list = List.of("Hello", "Reactive", "Programming");
+		
+		System.out.println("- flatMap");
+		//flatMap
+		Observable.fromIterable(list)
+		.flatMap(e -> Observable.fromArray(e.split("")))
+		.subscribe(System.out::println);
+		
+		System.out.println("- map");
+		//map
+		Observable.fromIterable(list)
+		.map(e -> e.length())
+		.subscribe(System.out::println);
+	}
+	
+}
