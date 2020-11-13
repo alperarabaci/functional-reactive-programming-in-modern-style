1ac9557864a37f853077ba7b88856039893c9ce2
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-11-13 11:13:40 +0300

    L126. Creating Observable

---
+++ section18-observable-and-observers/src/main/java/com/trainint/l126_creating_observable/CreatingObservableDemo.java
@@ -0,0 +1,34 @@
+package com.trainint.l126_creating_observable;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+
+public class CreatingObservableDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Observable.create:");
+		Observable<Integer> source = Observable.create(e -> {
+			e.onNext(10);
+			e.onNext(11);
+			e.onNext(12);
+			e.onComplete();
+		});
+		source.subscribe(System.out::println);
+		
+		
+		// just() Converts n items into an Observable that emits those items.
+		System.out.println("Observable.just:");
+		Observable<Integer> just = Observable.just(21,32,43);
+		just.subscribe(System.out::println);
+		
+		//fromIterable
+		System.out.println("Observable.fromIterable:");
+		List<String> list = List.of("Ram", "Shyam", "Mike");
+		
+		@NonNull
+		Observable<String> fromIterable = Observable.fromIterable(list);
+		fromIterable.subscribe(System.out::println);
+	}
+}
