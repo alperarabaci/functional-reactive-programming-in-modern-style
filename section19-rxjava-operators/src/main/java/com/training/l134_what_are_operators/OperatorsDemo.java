7f3111594fce0612349c02de52068869d00c3a17
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-11-13 14:59:54 +0300

    L134. What Are Operators?
    Filter & sort.

---
+++ section19-rxjava-operators/src/main/java/com/training/l134_what_are_operators/OperatorsDemo.java
@@ -0,0 +1,25 @@
+package com.training.l134_what_are_operators;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class OperatorsDemo {
+
+	public static void main(String[] args) {
+		
+		//filter
+		System.out.println("- filter:");
+		Observable.just(60, 57, 89, 90, 23)
+			.filter(e -> e>75)
+			.subscribe(e -> System.out.println("Grade A with " + e));
+		
+		
+		//filter & sorted
+		System.out.println("- filter & sorted:");
+		
+		Observable.just(60, 57, 89, 90, 23, 100, 74)
+		.filter(e -> e>75)
+		.sorted()
+		.subscribe(e -> System.out.println("Grade A with " + e));
+	}
+	
+}
